858dd5402658ed69c30083689bbd3bd70065ed4c
prenaissance <alexandru.andries@isa.utm.md>
2024-02-07 13:42:40 +0200

    fix: fix newline in serial

--- common/io.hpp
+++ common/io.hpp
@@ -4,10 +4,18 @@
 // https://forum.arduino.cc/t/printf-on-arduino/888528/3
 FILE f_out;
 
-int sput(char c, __attribute__((unused)) FILE *f) { return !Serial.write(c); }
+int sput(char c, __attribute__((unused)) FILE *f)
+{
+    if (c == '\n')
+    {
+        return !Serial.write("\r\n");
+    }
+    return !Serial.write(c);
+}
 
 void redirect_stdout()
 {
-    fdev_setup_stream(&f_out, sput, nullptr, _FDEV_SETUP_WRITE); //  https://www.nongnu.org/avr-libc/user-manual/group__avr__stdio.html#gaf41f158c022cbb6203ccd87d27301226
+    //  https://www.nongnu.org/avr-libc/user-manual/group__avr__stdio.html#gaf41f158c022cbb6203ccd87d27301226
+    fdev_setup_stream(&f_out, sput, nullptr, _FDEV_SETUP_WRITE);
     stdout = &f_out;
 }
